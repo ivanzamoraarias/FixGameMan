1e87cf97e701b9140e73c82c82b40e21e4462da6
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-01-31 20:14:02 +0100

    Simple Input Serie

---
+++ FixGameMan/Assets/Emil/code.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class code : MonoBehaviour
+{
+    public int[] sequence;
+    public Text sequenceText;
+    Slider slider;
+
+    private int currentIndex;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        sequenceText.text = "";
+        SetText();
+        slider.maxValue = sequence.Length;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("0 Input");
+            if(sequence[currentIndex] == 0)
+            {
+                AddToSlider();
+            }
+            else
+            {
+                Reset();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("1 Input");
+            if (sequence[currentIndex] == 1)
+            {
+                AddToSlider();
+            }
+            else
+            {
+                Reset();
+            }
+        }
+    }
+
+    void Reset()
+    {
+        slider.value = 0;
+        currentIndex = 0;
+    }
+
+    void AddToSlider()
+    {
+        slider.value += slider.maxValue/sequence.Length;
+        currentIndex++;
+
+        if(slider.value == slider.maxValue)
+        {
+            Debug.Log("Sequence completed...");
+        }
+
+    }
+
+    void SetText()
+    {
+        if (sequence.Length == 0)
+        {
+            Debug.Log("Array is empty...");
+            return;
+        }
+
+        foreach(int i in sequence)
+        {
+            sequenceText.text += i.ToString() + " ";
+        }
+    }
+}
---
+++ FixGameMan/Assets/Emil/input.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class input : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("0 Input");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("1 Input");
+        }
+    }
+}
