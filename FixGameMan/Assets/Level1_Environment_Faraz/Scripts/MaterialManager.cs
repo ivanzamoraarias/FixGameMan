17f9125b14e42ed645354687f37cc25be55cdc9c
Faraz <mohd.faraz.rashid@gmail.com>
2020-02-01 13:32:54 +0100

    Level 1 Environment

---
+++ FixGameMan/Assets/Level1_Environment_Faraz/Scripts/MaterialManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material originalMat;
+    public Material glitcgMat;
+    void Start()
+    {
+      
+    }
+
+    public void FixGlitchMaterial()
+    {
+        this.GetComponent<SpriteRenderer>().material = originalMat;
+        this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,0f);
+    }
+
+    public void OnMouseDown()
+    {
+        FixGlitchMaterial();
+    }
+    // Update is called once per frame
+    
+}
