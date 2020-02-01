7844e77b2828270be82297c36cebf64d192d1834
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-02-01 17:08:56 +0100

    GameOver and Timer prefab

---
+++ FixGameMan/Assets/Emil/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Text text;
+    public Image background;
+
+    void Start()
+    {
+        text.gameObject.SetActive(false);
+        background.gameObject.SetActive(false);
+
+        GameEvents.LooseTriggered += ShowGameOver;
+    }
+
+    void ShowGameOver()
+    {
+        text.gameObject.SetActive(true);
+        background.gameObject.SetActive(true);
+    }
+}
