a02d2461785faab8fcf5014f3b6cb47f025f9ae3
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-02-01 17:29:15 +0100

    GameOver load mainMenu

--- FixGameMan/Assets/Emil/Scripts/GameOver.cs
+++ FixGameMan/Assets/Emil/Scripts/GameOver.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class GameOver : MonoBehaviour
 {
     public Text text;
+    public Text clickText;
     public Image background;
 
     void Start()
     {
         text.gameObject.SetActive(false);
+        clickText.gameObject.SetActive(false);
         background.gameObject.SetActive(false);
 
         GameEvents.LooseTriggered += ShowGameOver;
@@ -19,6 +21,7 @@ public class GameOver : MonoBehaviour
     void ShowGameOver()
     {
         text.gameObject.SetActive(true);
+        clickText.gameObject.SetActive(true);
         background.gameObject.SetActive(true);
     }
 }
