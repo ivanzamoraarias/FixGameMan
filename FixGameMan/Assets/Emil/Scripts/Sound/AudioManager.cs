15a9875f51bbfd53b2fc1df5f679251b70ea8262
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-02-01 12:05:52 +0100

    AudioManger for sound files

---
+++ FixGameMan/Assets/Emil/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System;
+
+namespace GGJ.Sound
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager instance;
+        public Sound[] sounds;
+        public bool unmuteOnFocus = true;
+        public bool isMuted = false;
+
+        void Awake()
+        {
+            if (instance == null) instance = this;
+            else if (instance != this) Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+
+            foreach (Sound s in sounds)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.source.clip = s.clip;
+
+                s.source.volume = s.volume;
+                s.source.pitch = s.pitch;
+                s.source.loop = s.loop;
+            }
+        }
+
+        public void MuteSound()
+        {
+            foreach (Sound s in sounds)
+            {
+                s.source.volume = 0f;
+            }
+            isMuted = true;
+        }
+
+        public void UnmuteSound()
+        {
+            foreach (Sound s in sounds)
+            {
+                s.source.volume = s.volume;
+            }
+            isMuted = false;
+        }
+
+        public void Play(string name)
+        {
+            Sound s = Array.Find(sounds, sound => sound.name == name);
+
+            if (s == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found!");
+                return;
+            }
+
+            s.source.Play();
+        }
+
+        public void Stop(string name)
+        {
+            Sound s = Array.Find(sounds, sound => sound.name == name);
+
+            if (s == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found!");
+                return;
+            }
+
+            s.source.Stop();
+        }
+    }
+}
---
+++ FixGameMan/Assets/Emil/Scripts/Sound/Sound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GGJ.Sound
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        public bool loop;
+        [Range(0f, 3f)] public float volume;
+        [Range(.1f, 3f)] public float pitch;
+        [HideInInspector] public AudioSource source;
+    }
+}
