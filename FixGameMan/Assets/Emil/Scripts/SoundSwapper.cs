ad79a71a6773c4e3d266b0ea2740cc656ba6aee1
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-02-01 21:37:06 +0100

    SoundSwapper

---
+++ FixGameMan/Assets/Emil/Scripts/SoundSwapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GGJ.Sound;
+
+public class SoundSwapper : MonoBehaviour
+{
+    bool isFixed = false;
+
+    public string brokenSound;
+    public string fixedSound;
+    public int timeBetweenRepeat = 3;
+
+    public void StartLoop()
+    {
+        InvokeRepeating("PlaySound", 0, timeBetweenRepeat);
+    }
+
+    public void StopLoop()
+    {
+        CancelInvoke("PlaySound");
+    }
+
+    public void PlaySound()
+    {
+        if (isFixed)
+        {
+            AudioManager.instance.Play(fixedSound);
+        }
+        else
+        {
+            AudioManager.instance.Play(brokenSound);
+        }
+    }
+
+    public void FixSound()
+    {
+        AudioManager.instance.Stop(brokenSound);
+        isFixed = true;
+        CancelInvoke("PlaySound");
+        AudioManager.instance.Play(fixedSound);
+    }
+}
