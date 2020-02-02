5dbdfadd5bb410acb5a5e06d57dc888dc5f42f46
Faraz <mohd.faraz.rashid@gmail.com>
2020-02-02 12:18:35 +0100

    Level 1 Completely Fixed_12pm

--- FixGameMan/Assets/Asset Store/Realistic Glitches Lite/Assets/Scripts/ShaderEffect_CorruptedVram.cs
+++ FixGameMan/Assets/Asset Store/Realistic Glitches Lite/Assets/Scripts/ShaderEffect_CorruptedVram.cs
@@ -12,6 +12,7 @@ public class ShaderEffect_CorruptedVram : MonoBehaviour {
 	{
 		material = new Material( Shader.Find("Hidden/Distortion") );
 		texture = Resources.Load<Texture>("Checkerboard-big");
+		InvokeRepeating("Flicker",3.0f,2.0f);
 	}
 		
 	void OnRenderImage (RenderTexture source, RenderTexture destination)
@@ -19,5 +20,19 @@ public class ShaderEffect_CorruptedVram : MonoBehaviour {
 		material.SetFloat("_ValueX", shift);
 		material.SetTexture("_Texture", texture);
 		Graphics.Blit (source, destination, material);
+	}
+
+	void Update()
+	{
+		if(shift > -0.5f)
+		shift = Mathf.Lerp(shift,-0.5f, Time.deltaTime * 2f);
+		//else if(shift < -1.9f)
+		//shift = Mathf.Lerp(shift, 2f, Time.deltaTime * 1f);
+
+	}
+
+	void Flicker()
+	{
+		shift = 2f;
 	}
 }
